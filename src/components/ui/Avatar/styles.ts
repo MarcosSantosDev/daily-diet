94a2def9dad2a288d163bf8fcf6b8608a88c8ff8
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2024-06-16 18:41:53 -0300

    feat: create Avatar component

--- src/components/ui/Avatar/styles.ts
+++ src/components/ui/Avatar/styles.ts
@@ -4,11 +4,12 @@ export const AvatarContainer = styled.View`
   align-items: center;
   justify-content: center;
 `;
-
+    
 export const AvatarImage = styled.Image<{ size: number }>`
-  ${({ size }) => css`
+  ${({ theme, size }) => css`
     width: ${size}px;
     height: ${size}px;
+    border: 2px solid ${theme.colors.gray[2]};
     border-radius: ${size / 2}px;
   `}
 `;
