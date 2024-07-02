3d5e57feb9bc296502639026901f3a2c270cf60c
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2024-07-02 00:16:27 -0300

    feat: create meal registration feedback screen

--- src/screens/RegistrationFeedbackScreen/index.tsx
+++ src/screens/RegistrationFeedbackScreen/index.tsx
@@ -1,22 +1,43 @@
-import { Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { CheckCircle } from 'phosphor-react-native';
+
+import { Button } from '@/components/ui/Button';
 
 import * as S from './styles';
 
 export const RegistrationFeedbackScreen = () => {
   const navigation = useNavigation()
 
+  const positive = true;
+
   return (
     <S.Container>
-      <CheckCircle size={32} weight='fill' />
-      <S.Text>
-        Página de feedback de registro
-      </S.Text>
-      <Button
-        title="Voltar para pagina principal"
-        onPress={() => navigation.navigate('MealPanel')}
+      <S.Titles>
+        <S.Title variant={positive ? "positive" : "negative"}>
+          {positive ? "Continue assim!" : "Que pena!"}
+        </S.Title>
+        <S.TextOnRow>
+          {positive ? (
+            <S.Subtitle>Você continua <S.Bold> dentro da dieta</S.Bold> . Muito bem!</S.Subtitle>
+          ) : (
+            <S.Subtitle>Você<S.Bold> saiu da dieta</S.Bold>, dessa vez, mas continue se esforçando e não desista!</S.Subtitle>
+          )}
+        </S.TextOnRow>
+      </S.Titles>
+
+      <S.ImageIlustration
+        source={
+          positive
+            ? require('../../../assets/app/positiveIllustration.png')
+            : require('../../../assets/app/negativeIllustration.png')
+        }
       />
+
+      <Button
+        variant='primary'
+        onPress={() => navigation.navigate('MealPanel')}
+      >
+        Ir para a página inicial
+      </Button>
     </S.Container>
   );
 };
--- src/screens/RegistrationFeedbackScreen/styles.ts
+++ src/screens/RegistrationFeedbackScreen/styles.ts
@@ -9,12 +9,49 @@ export const Container = styled(SafeAreaView)`
 	background-color: ${({ theme }) => theme.colors.white[1]};
 	
   padding: 24px;
+  gap: 32px;
 `;
 
-export const Text = styled.Text`
+export const Titles = styled.View`
+  gap: 10px;
+`;
+
+export const Title = styled.Text<{ variant: 'positive' | 'negative' }>`
+  ${({ theme, variant }) => css`
+    text-align: center;
+    color: ${variant === 'positive' ? theme.colors.green[1] : theme.colors.red[1]}; 
+    font-family: ${theme.font.family.BOLD}; 
+    font-size: ${theme.font.size.X_LARGE}px; 
+  `}
+`;
+
+export const TextOnRow = styled.View`
+  flex-direction: row;
+`;
+
+export const Subtitle = styled.Text`
   ${({ theme }) => css`
+    text-align: center;
     color: ${theme.colors.gray[1]}; 
     font-family: ${theme.font.family.REGULAR}; 
-    font-size: ${theme.font.size.LARGE}px; 
+    font-size: ${theme.font.size.MD}px; 
+  `}
+`;
+
+export const Bold = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray[1]}; 
+    font-family: ${theme.font.family.BOLD}; 
+    font-size: ${theme.font.size.MD}px; 
+  `}
+`;
+
+export const ImageIlustration = styled.Image`
+${({ theme }) => css`
+    width: 224px;
+    height: 288px;
+    color: ${theme.colors.gray[1]}; 
+    font-family: ${theme.font.family.BOLD}; 
+    font-size: ${theme.font.size.MD}px; 
   `}
 `;
